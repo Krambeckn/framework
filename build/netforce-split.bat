6b849506f639869a498e7571a709fac707400e7a
Bruno Goncalves <bugotech@gmail.com>
2016-03-28 17:49:43 -0300

    Info build

--- build/netforce-split.bat
+++ build/netforce-split.bat
@@ -2,5 +2,9 @@ git subsplit.sh init git@github.com:netforcews/framework.git
 git subsplit.sh update
 
 git subsplit.sh publish --heads="master" src/Support:git@github.com:netforcews/support.git
+git subsplit.sh publish --heads="master" src/Validation:git@github.com:netforcews/validation.git
+git subsplit.sh publish --heads="master" src/Database:git@github.com:netforcews/database.git
+git subsplit.sh publish --heads="master" src/IO:git@github.com:netforcews/io.git
+git subsplit.sh publish --heads="master" src/Http:git@github.com:netforcews/http.git
 
 rd /S /Q .subsplit
