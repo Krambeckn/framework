61f873e6a18417f6469fef5c52a055361752329c
JohannLucas <johann.lucas@hotmail.com>
2016-04-07 15:00:50 -0300

    Testes do Support

--- tests/Support/PropertiesTest.php
+++ tests/Support/PropertiesTest.php
@@ -1,18 +1,42 @@
 <?php
 
-use NetForceWS\Support\Num;
+use NetForceWS\Support\Properties;
+use Carbon\Carbon;
 
 class PropertiesTest extends TestCase
 {
+
+    protected $proper;
+
     protected function setUp()
     {
-        include __DIR__ . '/Utils/Propriedades.php';
+        include_once __DIR__ . '/Utils/Propriedades.php';
+
+        $this->proper = new Propriedades();
     }
+
 
     public function testAttrNome()
     {
-        $values = new Propriedades();
-        $this->assertEquals('johann', $values->nome);
-        $this->assertEquals(null, $values->idade);//
+        $this->assertEquals('johann', $this->proper->nome);
+        $this->assertEquals(null, $this->proper->idade);
+    }
+
+    public function testFromJson()
+    {
+        $obj = new stdClass();
+        $obj->nome = 'Teste';
+        $this->assertEquals($obj, $this->proper->fromJson('{"nome": "Teste"}', true));
+        $this->assertEquals((array)$obj, $this->proper->fromJson('{"nome": "Teste"}', false));
+    }
+
+    public function testAsDateTime()
+    {
+        $carbon = Carbon::createFromFormat('d/m/Y', '15/02/2015')->startOfDay();
+
+        $this->assertEquals($carbon, $this->proper->datecarbon);
+        $this->assertEquals($carbon, $this->proper->dateymd);
+        $this->assertEquals($carbon, $this->proper->datetime);
+        $this->assertEquals($carbon, $this->proper->datenumber);
     }
 }
--- tests/Support/Utils/Propriedades.php
+++ tests/Support/Utils/Propriedades.php
@@ -8,4 +8,24 @@ class Propriedades
     {
         return 'johann';
     }
+
+    public function getDateymdAttribute()
+    {
+        return $this->asDateTime('2015-02-15');
+    }
+
+    public function getDatecarbonAttribute()
+    {
+        return $this->asDateTime(\Carbon\Carbon::createFromFormat('d/m/Y', '15/02/2015')->startOfDay());
+    }
+
+    public function getDateTimeAttribute()
+    {
+        return $this->asDateTime(strtotime('2015-02-15 00:00:00'));
+    }
+
+    public function getDatenumberAttribute()
+    {
+        return $this->asDateTime(1423958400);
+    }
 }
